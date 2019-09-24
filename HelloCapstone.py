0ef0754011373499af7a31f7670a0deefacd7170
Christopher Pearson <cmpear@uw.edu>
2019-09-24 15:06:17 -0700

    getting markdown added

--- HelloCapstone.py
+++ HelloCapstone.py
@@ -1,6 +1,10 @@
+#%% [markdown]
+# # imports pandas and numpy
+# ## then says hello to course
+
 #%%
 import pandas as pd
 import numpy as np
 
 #%%
-print("hello capstone project course")
+print("hello capstone project course!")
